4cce2ac807681be146dfcf666a24aea5f28405e6
Alec Mathisen <Alec-WAM@users.noreply.github.com>
2021-03-24 15:30:35 -0400

    NavMesh System

--- Assets/Scripts/HumanTestScript.cs
+++ Assets/Scripts/HumanTestScript.cs
@@ -5,19 +5,44 @@ using UnityEngine;
 public class HumanTestScript : MonoBehaviour
 {
     public Animator animator;
+    public GameObject aim_point;
     public int stealSeconds = 5;
+
+
+    UnityEngine.AI.NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
     {
-        animator.SetInteger("legs", 1);
-        animator.SetInteger("arms", 1);
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        animator.SetInteger("legs", 5);
+        animator.SetInteger("arms", 5);
     }
 
     //private bool sayingHello;
     // Update is called once per frame
     void Update()
     {
-        
+        if (aim_point != null)
+        {
+
+            if (Vector3.Distance(transform.position, aim_point.transform.position) > 0.25f)
+            {
+
+                agent.speed = 2.0f;
+                agent.SetDestination(aim_point.transform.position);
+                animator.SetInteger("arms", 1);
+                animator.SetInteger("legs", 1);
+            }
+
+            if (Vector3.Distance(transform.position, aim_point.transform.position) < 0.25f)
+            {
+                agent.speed = 0;
+
+                animator.SetInteger("arms", 5);
+                animator.SetInteger("legs", 5);
+            }
+        }
+
         if (Input.GetKeyDown(KeyCode.H))
         {
             waving_start = StartCoroutine(waving_execute());
@@ -26,10 +51,15 @@ public class HumanTestScript : MonoBehaviour
         {
             steal_start = StartCoroutine(steal_execute());
         }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            hang_start = StartCoroutine(hang_execute());
+        }
     }
 
     Coroutine waving_start;
     Coroutine steal_start;
+    Coroutine hang_start;
 
     IEnumerator waving_execute()
     {
@@ -46,6 +76,7 @@ public class HumanTestScript : MonoBehaviour
         StopCoroutine(waving_start);
     }
 
+
     IEnumerator steal_execute()
     {
         yield return new WaitForSeconds(0);
@@ -60,6 +91,23 @@ public class HumanTestScript : MonoBehaviour
         animator.SetInteger("legs", 1); //Walk
         animator.SetInteger("arms", 1); //Walk
         StopCoroutine(steal_start);
+    }
+
+    IEnumerator hang_execute()
+    {
+        yield return new WaitForSeconds(0);
+
+        int what_to_choose = UnityEngine.Random.Range(9, 12);
+
+        animator.SetInteger("legs", 1);
+        animator.SetInteger("arms", 8); //looking
+
+
+        yield return new WaitForSeconds(3);
+
+        animator.SetInteger("legs", 1);
+        animator.SetInteger("arms", 1); //Walk
+        StopCoroutine(waving_start);
     }
 
 }
