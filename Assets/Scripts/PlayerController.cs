24122931c76058794e5dee86d90e8c20ee277d57
Alec Mathisen <Alec-WAM@users.noreply.github.com>
2021-04-06 12:13:26 -0400

    Fixed Player Animaions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,18 +5,32 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public CharacterController characterController;
+    public Animator animator;
     public float speed = 6;
     public float rotationSpeed = 180.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator.SetInteger("legs", 5);
+        animator.SetInteger("arms", 5);
     }
 
+
+    private bool walking;
     // Update is called once per frame
     void Update()
     {
-        Move();   
+        Move();
+
+        if (walking)
+        {
+            animator.SetInteger("legs", 1);
+            animator.SetInteger("arms", 1);
+        } else
+        {
+            animator.SetInteger("legs", 5);
+            animator.SetInteger("arms", 5);
+        }
     }
 
     public void Move()
@@ -28,6 +42,14 @@ public class PlayerController : MonoBehaviour
 
         Vector3 move = new Vector3(0, 0, vertical * Time.deltaTime);
         move = this.transform.TransformDirection(move);
+
+        if(move != Vector3.zero)
+        {
+            walking = true;
+        } else
+        {
+            walking = false;
+        }
 
         characterController.Move(speed * move);
 
