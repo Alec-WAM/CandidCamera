af4c444543cd754747d892e791646277eba20e99
Alec Mathisen <Alec-WAM@users.noreply.github.com>
2021-04-08 15:46:07 -0400

    Agent Script Fixes

--- Assets/Scripts/VisualCameraAgentBlock1.cs
+++ Assets/Scripts/VisualCameraAgentBlock1.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
@@ -9,21 +10,31 @@ using Unity.MLAgents.Sensors;
 public class VisualCameraAgentBlock1 : Agent
 {
     private bool isJaywalking;
+
+
+    public Text debugInfo;
+    public int guesses;
+    public int hits;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (guesses > 0)
+        {
+            float hitRate = (float)hits / (float)guesses;
+            string text = "Accuracy: " + hitRate;
+            debugInfo.text = text;
+        }
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
-        sensor.AddObservation(isJaywalking);
+        
     }
 
     public override void OnActionReceived(ActionBuffers actions)
@@ -31,13 +42,17 @@ public class VisualCameraAgentBlock1 : Agent
         var discreteActions = actions.DiscreteActions;
 
         bool jWalkGuess = (int)discreteActions[0] > 0;
+        Debug.Log("Guessing " + discreteActions[0]);
 
         if (jWalkGuess)
         {
+            guesses++;
             if (isJaywalking)
             {
                 AddReward(1.0F);
                 Debug.Log("Caught");
+                hits++;
+                EndEpisode();
             }
             else
             {
@@ -49,12 +64,17 @@ public class VisualCameraAgentBlock1 : Agent
 
     public void triggerJaywalking()
     {
-        //Debug.Log("J-Walking!");
         isJaywalking = true;
     }
 
     public void stopJaywalking()
     {
         isJaywalking = false;
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var discreteActionsOut = actionsOut.DiscreteActions;
+        discreteActionsOut[0] = Input.GetKey(KeyCode.Space) ? 1 : 0;
     }
 }
